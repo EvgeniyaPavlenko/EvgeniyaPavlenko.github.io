a79561c2faaad924dbba12fa47301a0ae05d86d1
EvgeniyaPavlenko <evgeniyapavlenko@MacBook-Air-Evgeniya.local>
2016-01-20 00:02:54 +0200

    section certificates

--- Souzgel.ru/script/script.js
+++ Souzgel.ru/script/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+//SLIDER
+
 var labels = ["50м", "100м", "200м", "500м", "1км", "1.5км"]
 
   $("#slider").slider({
@@ -14,8 +16,6 @@ var labels = ["50м", "100м", "200м", "500м", "1км", "1.5км"]
       }
 
     })
-
-
   // Space out values
   for (var i = 0; i < labels.length; i++) {
 
@@ -24,8 +24,38 @@ var labels = ["50м", "100м", "200м", "500м", "1км", "1.5км"]
     $("#slider").append(el);
 
   }
-  
   $("#v1").text(labels[2]);
   $("#v2").text(labels[4]);
 
+  //POPUP
+
+$('.image-popup-vertical-fit').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    mainClass: 'mfp-img-mobile',
+    image: {
+      verticalFit: true
+    }
+  });
+  $('.image-popup-fit-width').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    image: {
+      verticalFit: false
+    }
+  });
+  $('.image-popup-no-margins').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
 });
