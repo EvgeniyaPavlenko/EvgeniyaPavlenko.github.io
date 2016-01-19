5b48cd8f47dc8aabfdd74d8a7aa22a204e408bde
EvgeniyaPavlenko <evgeniyapavlenko@MacBook-Air-Evgeniya.local>
2016-01-19 22:40:02 +0200

    slider

--- Souzgel.ru/script/script.js
+++ Souzgel.ru/script/script.js
@@ -1,16 +1,31 @@
- 
-  $(document).ready(function(){
-     $(function() {
-        $( "#slider-range" ).slider({
-          range: true,
-          min: 50,
-          max: 1500,
-          values: [ 50, 1500],
-          slide: function( event, ui ) {
-            $( "#amount" ).val( "м" + ui.values[ 0 ] + " - м" + ui.values[ 1 ] );
-          }
-        });
-        $( "#amount" ).val( "м" + $( "#slider-range" ).slider( "values", 0 ) +
-          " - м" + $( "#slider-range" ).slider( "values", 1 ) );
-      });
-  });
+$(document).ready(function(){
+
+var labels = ["50м", "100м", "200м", "500м", "1км", "1.5км"]
+
+  $("#slider").slider({
+      min: 0,
+      max: 5,
+      step: 1,
+      range: true,
+      values: [2, 4],
+      slide: function(event, ui) {
+        $("#v1").text(labels[ui.values[0]]);
+        $("#v2").text(labels[ui.values[1]]);
+      }
+
+    })
+
+
+  // Space out values
+  for (var i = 0; i < labels.length; i++) {
+
+    var el = $('<label>' + (labels[i]) + '</label>').css('left', (i / labels.length * 115) + '%');
+
+    $("#slider").append(el);
+
+  }
+  
+  $("#v1").text(labels[2]);
+  $("#v2").text(labels[4]);
+
+});
