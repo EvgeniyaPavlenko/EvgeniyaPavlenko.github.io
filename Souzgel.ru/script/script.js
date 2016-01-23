f901ffea33975e967ce195c778d8f44389541496
EvgeniyaPavlenko <evgeniyapavlenko@MacBook-Air-Evgeniya.local>
2016-01-23 12:10:00 +0200

    popup_visit

--- Souzgel.ru/script/script.js
+++ Souzgel.ru/script/script.js
@@ -111,3 +111,27 @@ var top_show = 150; // the position of the scroll bar to display a button
  
 });
 
+//POPUP FORM
+
+$(document).ready(function() {
+  $('.popup-with-form').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#name',
+
+    // When elemened is focused, some mobile browsers in some cases zoom in
+    // It looks not nice, so we disable it:
+    callbacks: {
+      beforeOpen: function() {
+        if($(window).width() < 700) {
+          this.st.focus = false;
+        } else {
+          this.st.focus = '#name';
+        }
+      }
+    }
+  });
+});
+
+
+
