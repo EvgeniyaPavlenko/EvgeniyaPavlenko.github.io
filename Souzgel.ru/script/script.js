dcd4d38236c1b3adaf49721a895c6bceadb52b7c
EvgeniyaPavlenko <evgeniyapavlenko@MacBook-Air-Evgeniya.local>
2016-01-26 19:01:57 +0200

    after_clients_comments

--- Souzgel.ru/script/script.js
+++ Souzgel.ru/script/script.js
@@ -58,19 +58,19 @@ $('.image-popup-vertical-fit').magnificPopup({
     }
   });
 
-  //OWL GALLERY_COMMENTS
+  // //OWL GALLERY_COMMENTS
 
-   $("#owl-demo").owlCarousel({
+  //  $("#owl-demo").owlCarousel({
  
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true,
-      dragBeforeAnimFinish : true,
-      mouseDrag : true,
-      touchDrag : true,
+  //     navigation : true, // Show next and prev buttons
+  //     slideSpeed : 300,
+  //     paginationSpeed : 400,
+  //     singleItem:true,
+  //     dragBeforeAnimFinish : true,
+  //     mouseDrag : true,
+  //     touchDrag : true,
  
-  });
+  // });
 
   //OWL GALLERY_POPUP
   $("#owl-demo_popup").owlCarousel({
@@ -95,23 +95,6 @@ $('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
       return false;
     });
 
-   //TOP BUTTON
-   var top_show = 150; // the position of the scroll bar to display a button
-  var delay = 1000; // scroll delay
-  $(document).ready(function() {
-    $(window).scroll(function () { // If you fall into this scrolling function
-      /* Depending on the position of the scroll bar, and the value top_show, close or open the "Top"*/
-      if ($(this).scrollTop() > top_show) $('#top').fadeIn();
-      else $('#top').fadeOut();
-    });
-    $('#top').click(function () { //Clicking on the "Back" get into this function
-      /* Smooth scrolling up */
-      $('body, html').animate({
-        scrollTop: 0
-      }, delay);
-    });
-  });
-
 //POPUP FORM
 
 $('.popup-with-form').magnificPopup({
@@ -131,4 +114,155 @@ $('.popup-with-form').magnificPopup({
       }
     }
   });
+
+
+//OWL NEW
+});
+
+$(document).ready(function() {
+ 
+  var sync1 = $("#sync1");
+  var sync2 = $("#sync2");
+ 
+  sync1.owlCarousel({
+    singleItem : true,
+    slideSpeed : 1000,
+    navigation: true,
+    pagination:false,
+    afterAction : syncPosition,
+    responsiveRefreshRate : 200,
+  });
+ 
+  sync2.owlCarousel({
+    items : 15,
+    itemsDesktop      : [1199,10],
+    itemsDesktopSmall     : [979,10],
+    itemsTablet       : [768,8],
+    itemsMobile       : [479,4],
+    pagination:false,
+    responsiveRefreshRate : 100,
+    afterInit : function(el){
+      el.find(".owl-item").eq(0).addClass("synced");
+    }
+  });
+ 
+  function syncPosition(el){
+    var current = this.currentItem;
+    $("#sync2")
+      .find(".owl-item")
+      .removeClass("synced")
+      .eq(current)
+      .addClass("synced")
+    if($("#sync2").data("owlCarousel") !== undefined){
+      center(current)
+    }
+  }
+ 
+  $("#sync2").on("click", ".owl-item", function(e){
+    e.preventDefault();
+    var number = $(this).data("owlItem");
+    sync1.trigger("owl.goTo",number);
+  });
+ 
+  function center(number){
+    var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+    var num = number;
+    var found = false;
+    for(var i in sync2visible){
+      if(num === sync2visible[i]){
+        var found = true;
+      }
+    }
+ 
+    if(found===false){
+      if(num>sync2visible[sync2visible.length-1]){
+        sync2.trigger("owl.goTo", num - sync2visible.length+2)
+      }else{
+        if(num - 1 === -1){
+          num = 0;
+        }
+        sync2.trigger("owl.goTo", num);
+      }
+    } else if(num === sync2visible[sync2visible.length-1]){
+      sync2.trigger("owl.goTo", sync2visible[1])
+    } else if(num === sync2visible[0]){
+      sync2.trigger("owl.goTo", num-1)
+    }
+    
+  }
+ 
+});
+
+$(document).ready(function() {
+ 
+  var sync1 = $("#sync1_popup");
+  var sync2 = $("#sync2_popup");
+ 
+  sync1.owlCarousel({
+    singleItem : true,
+    slideSpeed : 1000,
+    navigation: true,
+    pagination:false,
+    afterAction : syncPosition,
+    responsiveRefreshRate : 200,
+  });
+ 
+  sync2.owlCarousel({
+    items : 15,
+    itemsDesktop      : [1199,10],
+    itemsDesktopSmall     : [979,10],
+    itemsTablet       : [768,8],
+    itemsMobile       : [479,4],
+    pagination:false,
+    responsiveRefreshRate : 100,
+    afterInit : function(el){
+      el.find(".owl-item").eq(0).addClass("synced");
+    }
+  });
+ 
+  function syncPosition(el){
+    var current = this.currentItem;
+    $("#sync2_popup")
+      .find(".owl-item")
+      .removeClass("synced")
+      .eq(current)
+      .addClass("synced")
+    if($("#sync2").data("owlCarousel") !== undefined){
+      center(current)
+    }
+  }
+ 
+  $("#sync2_popup").on("click", ".owl-item", function(e){
+    e.preventDefault();
+    var number = $(this).data("owlItem");
+    sync1.trigger("owl.goTo",number);
+  });
+ 
+  function center(number){
+    var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+    var num = number;
+    var found = false;
+    for(var i in sync2visible){
+      if(num === sync2visible[i]){
+        var found = true;
+      }
+    }
+ 
+    if(found===false){
+      if(num>sync2visible[sync2visible.length-1]){
+        sync2.trigger("owl.goTo", num - sync2visible.length+2)
+      }else{
+        if(num - 1 === -1){
+          num = 0;
+        }
+        sync2.trigger("owl.goTo", num);
+      }
+    } else if(num === sync2visible[sync2visible.length-1]){
+      sync2.trigger("owl.goTo", sync2visible[1])
+    } else if(num === sync2visible[0]){
+      sync2.trigger("owl.goTo", num-1)
+    }
+    
+  }
+ 
 });
